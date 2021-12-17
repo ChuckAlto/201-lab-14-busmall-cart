7273e4e1540208defce15823af3e3c0f1330e878
Charles Altopiedi <chuckalto@gmail.com>
2021-12-16 19:51:44 -0500

    populated the form, added event.preventdefault, added add selecteditemto cart

--- js/catalog.js
+++ js/catalog.js
@@ -9,10 +9,14 @@ const cart = new Cart([]);
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
 
-  //TODO: Add an <option> tag inside the form's select for each product
+  //done: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in Product.allProducts) {
-
+    let optionElement = document.createElement('option');
+    optionElement.textContent = Product.allProducts[i].name;
+    optionElement.value = Product.allProducts[i].name;
+    selectElement.appendChild(optionElement);
+    console.log(selectElement);
   }
 
 }
@@ -22,28 +26,45 @@ function populateForm() {
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
 
-  // TODO: Prevent the page from reloading
-
+  // done: Prevent the page from reloading
+  event.preventDefault();
+  // console.log(event.target.value.name);
   // Do all the things ...
   addSelectedItemToCart();
   cart.saveToLocalStorage();
   updateCounter();
   updateCartPreview();
-
+  
 }
 
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
-  // TODO: suss out the item picked from the select list
-  // TODO: get the quantity
-  // TODO: using those, add one item to the Cart
+  let product = document.getElementById('items').value;
+  console.log(product);
+  let quantity = document.getElementById('quantity').value;
+  console.log(quantity);
+  // done: suss out the item picked from the select list
+  // done: get the quantity
+  // done: using those, add one item to the Cart
+  cart.addItem(product, quantity);
+  console.log(cart);
 }
 
-// TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
-
+// done: Update the cart count in the header nav with the number of items in the Cart
+function updateCounter() {
+  let cartCount = document.getElementById('itemCount');
+  cartCount.textContent = `:  ${cart.items.length}` ;
+  
+}
+  
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
+  let header = document.getElementsByName('header');
+  let h2 = document.createElement('h2');
+  h2.textContent = `${cart.items[0]} ${cart.items[1]}`;
+  header.appendChild(h2);
+  // this doesn't work.
+
   // TODO: Get the item and quantity from the form
   // TODO: Add a new element to the cartContents div with that information
 }
